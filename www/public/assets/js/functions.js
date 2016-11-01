4b8549fcf1daa202dc458be0ca834c0363dc0fc8
jlucio <jlucio@abargon.com>
2016-11-01 12:53:49 -0600

    slider auto

--- www/public/assets/js/functions.js
+++ www/public/assets/js/functions.js
@@ -153,7 +153,8 @@
 			infinite: false,
 			touchMove: false,
 			swipeToSlide: false,
-			// autoplay: true,
+			autoplay: true,
+  			autoplaySpeed: 3000,
 		});
 
 		$('.slider-intro .slick-dots').wrap('<div class="slick-dots-wrapper"><div class="shell"></div></div>')
